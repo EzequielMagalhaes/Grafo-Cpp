84a6bfa0748ab0eb3a1a5464cc32344382db1b3e
Ezequiel Magalhaes <zekes_243@hotmail.com>
2022-12-19 23:07:11 -0300

    Atualização do menu - 19.12.22

--- Grafo.cpp
+++ Grafo.cpp
@@ -3,13 +3,26 @@
 #include<stdlib.h> //Standard Library para usar o comando de limpar a tela (system("cls"))
 int Menu(){
   system("cls");
-  printf("\n ====== MENU DE CONTROLE =====");
+  printf("\n ====== MENU DE CONTROLE ======");
+  printf("\n = 1: Inicializar a Matriz 	  =");
+  printf("\n = 2: Imprimir a Matriz 	  =");
+  printf("\n = 3: Inserir valor na Matriz 	  =");
+  printf("\n = 4: Percorrer a Matriz 	  =");
+  printf("\n = 5: Identificar menor 	  =");
+  printf("\n = 6: Sair do programa 		  =");
+  printf("\n ==============================");
+  printf("\n Digite a opcao desejada: ");
+  int resposta;
+  scanf("%i", &resposta);
+  return(resposta);
 }
 
 int main(){
-  int tamanho;
-	printf ("Informe o tamanho do grafo: ");
-	scanf("%i", &tamanho);
-	printf("Foi informado o tamanho do grafo: %i", tamanho);
+	int resposta;
+	do{
+		resposta = Menu();
+	}while(resposta != 6);
+
+  	
 	getch(); //Comando para guardar o pressionamento de uma tecla (na biblioteca conio.h)
 }
